e9b09b466319914095661f930ff74df38a32f05c
zim <zim@cigoos.com>
2020-09-16 14:58:37 +0800

    better

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -9,6 +9,10 @@ class Index
 {
     public function index()
     {
+        dump(
+            env('APP_DEBUG'),
+            env('cigo_admin.check_sign')
+        );
 //        return '您好！这是一个[index]示例应用';
         return View::fetch();
     }
